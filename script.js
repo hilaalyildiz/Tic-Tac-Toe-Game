719ff13ae1d5aedb8c9c25d02ee465e118c2ca33
hilaalyildiz <75807952+hilaalyildiz@users.noreply.github.com>
2022-05-10 15:07:56 +0300

    Tic Tac Toe Game

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const selectBox = document.querySelector(".select-box"),
 selectXBtn = selectBox.querySelector(".playerX"),
 selectOBtn = selectBox.querySelector(".playerO"),
 playBoard = document.querySelector(".play-board"),
-allBox = document.querySelectorAll("section span");
+allBox = document.querySelectorAll("section span"),
+players = document.querySelector("players");
 
 
 window.onload = () => { // once window load
@@ -20,5 +21,23 @@ window.onload = () => { // once window load
     selectOBtn.onclick = () => {
         selectBox.classList.add("hide"); // hide the select-box on playerO button clicked
         playBoard.classList.add("show"); // show the playboard section on playerX button clicked
+        players.setAttribute("class","players avtive player"); // adding three class names in player element
     }
+}
+
+
+let playerXIcon = "fas fa-times"; // class name of fontawesome cross icon
+let playerOIcon = "far fa-circle"; // class name of fontawesome circle icon
+
+// user clicked function
+function clickedBox(element){
+    if(players.classList.contains("player")){ // if players element has contains .player
+        element.innerHTML=`<i class="${playerOIcon}"></i>`; // adding cross icon tag inside user clicked element
+        players.classList.add("active");
+    } else {
+        element.innerHTML=`<i class="${playerXIcon}"></i>`; // adding circle icon tag inside user clicked element
+        players.classList.add("active");
+    }
+
+    element.style.pointerEvents="none"; // once user select any boc then that box can't be selected again
 }
