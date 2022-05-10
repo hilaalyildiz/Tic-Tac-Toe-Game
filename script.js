404fcee0654458806cca9ae94fafbdde1a9b3377
hilaalyildiz <75807952+hilaalyildiz@users.noreply.github.com>
2022-05-10 14:16:37 +0300

    Tic Tac Toe Game

--- script.js
+++ script.js
@@ -2,12 +2,23 @@
 const selectBox = document.querySelector(".select-box"),
 selectXBtn = selectBox.querySelector(".playerX"),
 selectOBtn = selectBox.querySelector(".playerO"),
-playBoard = document.querySelector(".play-board");
+playBoard = document.querySelector(".play-board"),
+allBox = document.querySelectorAll("section span");
 
 
 window.onload = () => { // once window load
+
+    for (let i = 0; i < allBox.length; i++) { // add onclick attribute in all avaiable section's spans
+        allBox[i].setAttribute("onclick","clickedBox(this)");
+    }
+
     selectXBtn.onclick = () => {
         selectBox.classList.add("hide"); // hide the select-box on playerX button clicked
+        playBoard.classList.add("show"); // show the playboard section on playerX button clicked
+    }
+
+    selectOBtn.onclick = () => {
+        selectBox.classList.add("hide"); // hide the select-box on playerO button clicked
         playBoard.classList.add("show"); // show the playboard section on playerX button clicked
     }
 }
