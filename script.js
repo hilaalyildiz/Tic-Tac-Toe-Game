f2b8bfccdf9ce8eec02f45462952f410e5a3d7cf
hilaalyildiz <75807952+hilaalyildiz@users.noreply.github.com>
2022-05-10 15:48:43 +0300

    Tic Tac Toe Game

--- script.js
+++ script.js
@@ -28,16 +28,62 @@ window.onload = () => { // once window load
 
 let playerXIcon = "fas fa-times"; // class name of fontawesome cross icon
 let playerOIcon = "far fa-circle"; // class name of fontawesome circle icon
+let playerSign ="X"; // suppose player will be X
 
 // user clicked function
 function clickedBox(element){
     if(players.classList.contains("player")){ // if players element has contains .player
-        element.innerHTML=`<i class="${playerOIcon}"></i>`; // adding cross icon tag inside user clicked element
+        playerSign = "O";// if player will be O then we'll change the sign
+        element.innerHTML=`<i class="${playerOIcon}"></i>`; // adding circle icon tag inside user clicked element
         players.classList.add("active");
+
+        // iff player select O then we'll change the playerSign value to O
+        playerSign="O";
+        element.setAttribute("id",playerSign);
+
     } else {
-        element.innerHTML=`<i class="${playerXIcon}"></i>`; // adding circle icon tag inside user clicked element
+        element.innerHTML=`<i class="${playerXIcon}"></i>`; // adding cross icon tag inside user clicked element
         players.classList.add("active");
+        element.setAttribute("id",playerSign);
     }
 
-    element.style.pointerEvents="none"; // once user select any boc then that box can't be selected again
+    element.style.pointerEvents="none"; // once user select any box then that box can't be selected again
+    let randomDelayTime = ((Math.random()*1000)+200).toFixed(); // generating random time delay so bot will delay randomly to select box
+    setTimeout(()=>{
+        bot(); // calling bot function
+    },randomDelayTime); //passing random delay time
+}
+
+// bot click function
+function bot(){
+    // first change the playerSign .. so if user has X value in id then bot will have O
+    playerSign="O";
+    let array = []; // creating empty array.. we'll store unselected box index in this array
+    for (let i = 0; i < allBox.length; i++) {
+        if(allBox[i].childElementCount==0){ // if span has no any child element
+            array.push(i); // inserting unclicked or unselected boxes inside array means that span has no children 
+            console.log(i + " "+" has no children");
+        }
+        
+    }
+
+    let randomBox = array[Math.floor(Math.random()* array.length)]; 
+    // getting random ,ndex from array so bot will will select random unselected box 
+
+    if(array.length>0){
+        if(players.classList.contains("player")){ // if players element has contains .player
+            allBox[randomBox].innerHTML=`<i class="${playerXIcon}"></i>`; // adding cross icon tag inside user clicked element
+            players.classList.remove("active");
+            playerSign = "X";
+            allBox[randomBox].setAttribute("id",playerSign);
+        } else {
+            allBox[randomBox].innerHTML=`<i class="${playerOIcon}"></i>`; // adding circle icon tag inside user clicked element
+            players.classList.remove("active");
+            allBox[randomBox].setAttribute("id",playerSign);
+        }
+    
+    } 
+
+    allBox[randomBox].style.pointerEvents="none"; // once bot select any box then user can't select or click on that box
+
 }
